fd9ae53ed6280f27ac0c775a91dd54b11532dbed
Руслан Чекпаков <ya.nechit@mail.ru>
2023-02-23 09:25:50 +0600

    add working wersion without classes

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from random import randint
+from graphic_arts.graphic_arts.start_game_banner import run_screensaver
 
 attact_defolt = 'нанёс урон противнику равный'
 special_defolt = 'применил специальное умение'
 
 
 def attack(char_name: str, char_class: str) -> str:
+    """Выполняет команду атаки."""
     if char_class == 'warrior':
         return (f'{char_name} {attact_defolt} {5 + randint(3, 5)}')
     if char_class == 'mage':
@@ -15,6 +17,7 @@ def attack(char_name: str, char_class: str) -> str:
 
 
 def defence(char_name: str, char_class: str) -> str:
+    """Выполняет команду блокирования."""
     if char_class == 'warrior':
         return (f'{char_name} блокировал {10 + randint(5, 10)} урона')
     if char_class == 'mage':
@@ -25,6 +28,7 @@ def defence(char_name: str, char_class: str) -> str:
 
 
 def special(char_name: str, char_class: str) -> str:
+    """Выполняет команду специального умения."""
     if char_class == 'warrior':
         return (f'{char_name} {special_defolt} «Выносливость {80 + 25}»')
     if char_class == 'mage':
@@ -35,6 +39,7 @@ def special(char_name: str, char_class: str) -> str:
 
 
 def start_training(char_name, char_class):
+    """Выполняет команду начала тренировки."""
     if char_class == 'warrior':
         print(f'{char_name}, ты Воитель — отличный боец ближнего боя.')
     if char_class == 'mage':
@@ -60,6 +65,7 @@ def start_training(char_name, char_class):
 
 
 def choice_char_class():
+    """Приветсвие игрока и запрос имени."""
     approve_choice: str = ''
     approve_choice = None
     char_class = None
@@ -83,18 +89,16 @@ def choice_char_class():
     return char_class
 
 
-def main():
+if __name__ == '__main__':
+    run_screensaver()
     print('Приветствую тебя, искатель приключений!')
     print('Прежде чем начать игру...')
-    char_name = input('...назови себя: ')
-    char_name: str
+    char_name: str = input('...назови себя: ')
     print(f'Здравствуй, {char_name}! '
           'Сейчас твоя выносливость — 80, атака — 5 и защита — 10.')
     print('Ты можешь выбрать один из трёх путей силы:')
     print('Воитель, Маг, Лекарь')
-    char_class = choice_char_class()
-    char_class: str
+    char_class: str = choice_char_class()
     print(start_training(char_name, char_class))
-
 
 main()
--- temp.py
+++ temp.py
@@ -1,44 +1,68 @@
-# Тестовые данные.
-TEST_DATA = [
-    (44, 'success', True),
-    (16, 'failure', True),
-    (4, 'success', False),
-    (21, 'failure', False),
-]
-TEST_DATA: list[tuple[int, str, bool]]
+fclass Sword:
+
+    def __init__(self, name, material, blade_length, grip):
+        self.name = name
+        self.blade_length = blade_length
+        self.material = material
+        self.grip = grip
+        print(f'Новый меч {name} выкован!')
+    
+    def slashing_blow(self):
+        return (f'Нанесён рубящий удар мечом {self.name}. '
+                f'Радиус поражения: {self.blade_length}.')
+
+    def piercing_strike(self):
+        return (f'Нанесён пронзающий удар мечом {self.name}. '
+                f'Рукоять {self.grip} мягко легла в руку.')
+
+    def sharpen(self):
+        return (f'Меч "{self.name}" заточен,'
+                f' {self.material} отлично поддалась обработке.')
+
+# Создаём экземпляр класса Sword.
+first_sword = Sword('Тренировочный',
+                    'Кора железного дуба',
+                    1.2, 'хват одной рукой')
+
+print('Перед вами манекен, опробуйте удары на нём.')
+# Вызвать метод рубящего удара.
+print(first_sword.slashing_blow())
+# Вызвать метод пронзающего удара.
 
 
-BONUS = 1.1
-BONUS: float = 1.1
-ANTIBONUS = 0.8
-ANTIBONUS: float = 0.8
 
 
-def add_rep(current_rep: float, rep_points: int, buf_effect: bool) -> float:
-    current_rep += rep_points
-    if buf_effect:
-        return current_rep * BONUS
-    return current_rep
+class Sword:
+    # Объявляем конструктор класса и указываем все необходимые параметры: 
+    # обязательный параметр self, название меча, материал клинка, 
+    # длину клинка, тип рукояти.
+    def __init__(self, name, material, blade_length, grip):
+        # Объявляем свойства класса и передаём в них необходимые значения.
+        self.name = name
+        self.blade_length = blade_length
+        self.material = material
+        self.grip = grip
+        # Когда объект будет создан, выведется сообщение.
+        print(f'Новый меч {name} выкован!')
+
+    # Объявляем собственные методы,
+    # в которых используются значения свойств класса Sword.
+    # Метод «Нанести рубящий удар».
+    def slashing_blow(self):
+        return (f'Нанесён рубящий удар мечом {self.name}. '
+                f'Радиус поражения: {self.blade_length}.')
+    
+    # Метод «Выполнить укол».
+    def piercing_strike(self):
+        return (f'Нанесён пронзающий удар мечом {self.name}. '
+                f'Рукоять {self.grip} мягко легла в руку.')
+
+    # Метод «Заточить клинок».
+    def sharpen(self):
+        return (f'Меч "{self.name}" заточен,'
+                f' {self.material} отлично поддалась обработке.') 
 
 
-def remove_rep(current_rep: float, rep_points: int,
-               debuf_effect: bool) -> float:
-    current_rep -= rep_points
-    if debuf_effect:
-        return current_rep * ANTIBONUS
-    return current_rep
 
-
-def main(duel_res: list[tuple[TEST_DATA]]):
-    current_rep = 0.0
-    for rep, result, effect in duel_res:
-        if result == 'success':
-            current_rep = add_rep(current_rep, rep, effect)
-        if result == 'failure':
-            current_rep = remove_rep(current_rep, rep, effect)
-    return (f'После {len(duel_res)} поединков, репутация персонажа'
-            f' — {current_rep:.3f} очков.')
-
-
-# Тестовый вызов функции main.
-print(main(TEST_DATA))
+                self.start_time = None
+        self.end_time = None
